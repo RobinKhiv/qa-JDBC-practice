139c6fc8c8b3b19024be43035e7f1aec53bcb51f
Robin Khiv <robinkhiv@gmail.com>
2021-05-26 13:11:35 -0700

    rk:added connection to db

--- src/main/java/com/qa/main/db/DBconnection.java
+++ src/main/java/com/qa/main/db/DBconnection.java
@@ -1,5 +1,19 @@
 package com.qa.main.db;
 
-public class DBconnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.qa.main.utils.DBconfig;
+
+public class DBconnection {
+	private PreparedStatement ps;
+	private Connection con;
+	private ResultSet rs;
+	
+	public DBconnection() throws SQLException {
+		con = DriverManager.getConnection(DBconfig.url, DBconfig.user, DBconfig.pw);
+	}
 }
